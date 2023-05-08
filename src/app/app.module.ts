84acbb09b6a898caf77f1f0c4893078e0b5b835d
Lukas Gimberis <lukasgimberis@gmail.com>
2023-05-08 13:15:51 +0100

    Revert "Add a Player component and draw it on the page with no current link to the maze."
    This reverts commit 1e1a4f5c845877122a425e5747da80e3c26b012e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MazeComponent } from './maze/maze.component';
 import { ControlsComponent } from './controls/controls.component';
-import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MazeComponent,
-    ControlsComponent,
-    PlayerComponent
+    ControlsComponent
   ],
   imports: [
     BrowserModule
