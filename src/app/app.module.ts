b9473b078cd538358987d9c2d797e5286cd523c6
Lukas Gimberis <lukasgimberis@gmail.com>
2023-05-08 11:53:08 +0100

    Add the Controls component, with arrows drawn to correspond to each control.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MazeComponent } from './maze/maze.component';
+import { ControlsComponent } from './controls/controls.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MazeComponent
+    MazeComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule
