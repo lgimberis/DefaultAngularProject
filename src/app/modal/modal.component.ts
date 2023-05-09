6d8cfdb360677115e0cf4ab9bb3d2f425876cd74
Lukas Gimberis <lukasgimberis@gmail.com>
2023-05-09 14:11:48 +0100

    Highlight answers when they are clicked, play their sound, and enable the 'confirm' button - which is now disabled until an answer is selected.

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -21,8 +21,11 @@ export class ModalComponent {
   } 
   question: Question = {id: '-1', text: '', audio: '', competency: '', options: []};
   answers: Answer[] = [];
+  answerClasses: string[] = [];
+  buttonClass: string = '';
   validIDs: number[] = Array(Questions.length).fill(0).map((x, i) => i);
   modalButtonText: string = 'Exit';
+  modalButtonDisabled: string = 'disabled'
   chooseRandomQuestion = () => {
     if (this.validIDs.length == 0) {
       console.error("Too many question boxes placed - out of questions!")
@@ -31,14 +34,35 @@ export class ModalComponent {
     let id: number = (this.validIDs.splice(Math.floor(Math.random() * this.validIDs.length), 1))[0];
     this.question = Questions[id];
 
-    //Shuffle answers randomly (sort according to random values assigned to each index)
+    // Shuffle answers randomly (sort according to random values assigned to each index)
     this.answers = this.question.options
       .map(v => ({v, 'sort': Math.random()}))
       .sort((a, b) => (a.sort - b.sort))
       .map(({v}) => v);
-    let audio = new Audio();
-    audio.src = "../../assets/Assignment Assets/Audios/" + this.question.audio.toLowerCase() + ".mp3";
+
+    this.answerClasses = Array(this.answers.length).fill('');
+    // Play the question audio
+    let audio = new Audio("../../assets/Assignment Assets/Audios/" + this.question.audio.toLowerCase() + ".mp3");
     audio.load();
     audio.play();
+  }
+  selectAnswer = (index: number) => {
+    // Apply 'selected' CSS to chosen answer
+    for(let i = 0; i < this.answerClasses.length; i++) {
+      if (i == index) {
+        this.answerClasses[i] = 'question-answer-selected';
+      } else {
+        this.answerClasses[i] = '';
+      }
+    }
+    // Play the selection audio of chosen answer
+    let audio = new Audio("../../assets/Assignment Assets/Audios/" + this.answers[index].audio.toLowerCase() + ".mp3");
+    audio.load();
+    audio.play();
+    // Enable the 'Confirm' button
+    this.modalButtonDisabled = '';
+  }
+  confirmAnswer = () => {
+
   }
 }
